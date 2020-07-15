a657a754686c05ce8cbcdf99a69703d6e43f8efe
Joshua Flark <joshuajmark@gmail.com>
2020-07-15 12:15:46 -0700

    first commit

---
+++ db/migrate/00000000000001_create_accounts.rb
@@ -0,0 +1,9 @@
+class CreateAccounts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :accounts, force: true do |t|
+      t.string :name, null: false
+      t.string :subdomain, null: false
+      t.timestamps
+    end
+  end
+end
